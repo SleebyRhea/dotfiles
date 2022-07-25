71790a462524c96c2e46b05db8993b01931311dc
Alison Wyatt <sleepyfugu@gmail.com>
2022-07-25 19:04:08 -0400

    Fix derp

--- env/profiled/00-micro.sh
+++ env/profiled/00-micro.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 declare micro_loc
 if micro_loc="$(command -v micro 2>/dev/null)"
-	then alias micro="${micro_loc} -config-dir '${HOME}/Application Support/Micro-Editor'"
+then
+	alias micro="${micro_loc} -config-dir '${HOME}/Library/Application Support/Micro-Editor'"
+	mkdir "${HOME}/Library/Application Support/Micro-Editor"
 fi
 unset micro_loc
