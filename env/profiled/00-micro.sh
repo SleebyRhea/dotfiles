e149004060f6b1d8aac035d2f7e8127f38ab8db4
Alison Wyatt <sleepyfugu@gmail.com>
2022-07-26 13:17:06 -0400

    Update 00-micro.sh

--- env/profiled/00-micro.sh
+++ env/profiled/00-micro.sh
@@ -3,6 +3,6 @@ declare micro_loc
 if micro_loc="$(command -v micro 2>/dev/null)"
 then
 	alias micro="${micro_loc} -config-dir '${HOME}/Library/Application Support/Micro-Editor'"
-	mkdir "${HOME}/Library/Application Support/Micro-Editor"
+	mkdir -p "${HOME}/Library/Application Support/Micro-Editor"
 fi
 unset micro_loc
